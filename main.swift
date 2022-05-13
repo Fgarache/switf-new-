7cbfc208105fd3bfe28e45ff070d4dd1ce2e5834
Francisco Garache <franciscogarach3@gmail.com>
2022-05-13 17:44:36 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,9 @@
+//import UIkit
+
+var str = "Hello, World!";
+print(str);
+let full = 3;
+var current=0
+var x = 0.2, y = 0.1, z = 0.2;
+
+print("El numero es , \(current) de un total disponible \(full)")
